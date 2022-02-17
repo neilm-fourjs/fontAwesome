0eb87753f11ceabd49df504ad970cd297e842ba9
Neil Martin <danteuk@gmail.com>
2022-02-17 17:03:57 +0000

    updated

---
+++ etc/getNew.sh
@@ -0,0 +1,11 @@
+NEW=new
+NEWF=$NEW/material-design-icons/font/
+NEWWF=$NEW/mdi/node_modules/@mdi/font/fonts/
+cp $NEW/gmdi.txt .
+cp $NEWWF/materialdesignicons-webfont.ttf .
+cp $NEWF/MaterialIconsOutlined-Regular.otf .
+cp $NEWF/MaterialIcons-Regular.ttf .
+cp $NEWF/MaterialIconsRound-Regular.otf .
+cp $NEWF/MaterialIconsTwoTone-Regular.otf .
+cp $NEWF/MaterialIconsTwoTone-Regular.woff2 .
+
